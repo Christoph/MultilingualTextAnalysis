3141a8db7b9efedba1ce74e26f1601f40bfe2066
Christoph Kralj <christophkralj@MacBook-Pro.local>
2020-06-09 15:18:07 +0200

    prepare datastet method

--- classification.py
+++ classification.py
@@ -16,66 +16,72 @@ from sklearn.preprocessing import MultiLabelBinarizer
 def preprocess_text(text):
     # replace non characers with space
     # regexr = re.sub(r"[^a-zA-Z0-9.!? ]+", " ", text)
-    temp = re.sub(r"[^a-zA-Z0-9 ]+", " ", text.lower())
+    temp = re.sub(r"[/W/D/S.,-]+", " ", text.lower())
     # merge multiple spaces to a single one
     return re.sub(r"[ ]+", " ", temp)
 
 
-language = 'hu'
+# language = 'es'
 
-# Load data
-data = pd.read_csv('data/articles_dictionary_annotated_'+language+'.csv')
-
-# Preprocess text
-clean_data = data['all_text_orig_lang_lemma'].apply(
-    lambda x: preprocess_text(x))
-
-# Target variables
+# # Load data
+# data = pd.read_csv('data/articles_dictionary_annotated_'+language+'.csv')
 
 
-def label_converter(x):
-    temp = []
-    if x['d_fr_eco']:
-        temp.append('d_fr_eco')
-    if x['d_fr_lab']:
-        temp.append('d_fr_lab')
-    if x['d_fr_sec']:
-        temp.append('d_fr_sec')
-    return temp
+def prepare_datasets(language, target, sampling, max_df_list):
+    data = pd.read_csv('data/articles_dictionary_annotated_'+language+'.csv')
+    output = []
+
+    # Preprocess text
+    data['clean'] = data['all_text_orig_lang_lemma'].apply(
+        lambda x: preprocess_text(x))
+
+    positive = data[data[target] == 1]
+    negative = data[data[target] == 0]
+
+    for (positive_count, negative_count) in sampling:
+        try:
+            sample = positive.sample(positive_count)
+            sample = sample.append(negative.sample(negative_count))
+
+            for maxdf in max_df_list:
+                tfidf = TfidfVectorizer(max_df=maxdf)
+                vecs = tfidf.fit_transform(sample['clean'])
+                target_vec = sample[target]
+
+                output.append([
+                    ' '.join([
+                        language,
+                        target,
+                        'tfidf-'+str(maxdf),
+                        str(positive_count),
+                        str(negative_count)
+                    ]),
+                    vecs,
+                    target_vec
+                ])
+        except:
+            print('Too many samples:', target, sampling, max_df_list)
+
+    return output
 
 
-all_classes = data.apply(label_converter, axis=1)
-mlb = MultiLabelBinarizer()
-y_data = mlb.fit_transform(all_classes)
-
-# Vectorization
-tfidf = TfidfVectorizer(max_df=0.8)
-vecs = tfidf.fit_transform(clean_data)
-
-# SVM
-svm = SVC()
-svm.fit(vecs, data['d_fr_eco'])
-
-# classification
-datasets = [
-    ["tfidf 0.8", vecs]
+sampling = [
+    [1000, 1000],
+    [500, 1000],
+    [500, 500],
+    [250, 250],
 ]
-
-targets = [
-    ['eco', data['d_fr_eco']],
-    ['lab', data['d_fr_lab']],
-    ['sec', data['d_fr_sec']],
-]
+tfidf_maxdf = [0.5, 0.7, 0.9]
 
 classifications = [
-    # ["DecisionTree", DecisionTreeClassifier, [
-    #     # {"criterion": "gini", "min_samples_split": 0.01},
-    #     # {"criterion": "entropy", "min_samples_split": 0.01},
-    #     # {"criterion": "gini", "min_samples_split": 0.05},
-    #     # {"criterion": "entropy", "min_samples_split": 0.05},
-    #     # {"criterion": "gini"},
-    #     # {"criterion": "entropy"},
-    # ]],
+    ["DecisionTree", DecisionTreeClassifier, [
+        {"criterion": "gini", "min_samples_split": 0.01},
+        {"criterion": "entropy", "min_samples_split": 0.01},
+        {"criterion": "gini", "min_samples_split": 0.05},
+        {"criterion": "entropy", "min_samples_split": 0.05},
+        # {"criterion": "gini"},
+        # {"criterion": "entropy"},
+    ]],
     # ["AdaBoost", AdaBoostClassifier, [
     #     {"n_estimators": 25, "learning_rate": 1},
     #     # {"n_estimators": 25, "learning_rate": 0.5},
@@ -91,22 +97,22 @@ classifications = [
     #     # {"n_estimators": 200},
     #     # {"n_estimators": 300},
     # ]],
-    ["SVM", SVC, [
-        {"gamma": "scale", "kernel": "rbf"},
-        {"gamma": "scale", "kernel": "linear"},
-    ]],
-    # ["Random Forest", RandomForestClassifier, [
-    #     #     {"n_estimators": 200, "criterion": "entropy", "min_samples_split": 0.01},
-    #     #     {"n_estimators": 200, "criterion": "entropy", "min_samples_split": 0.05},
-    #     {"n_estimators": 100, "criterion": "gini"},
-    #     {"n_estimators": 100, "criterion": "entropy"},
-    #     # {"n_estimators": 200, "criterion": "gini"},
-    #     #     {"n_estimators": 200, "criterion": "entropy"},
-    #     #     {"n_estimators": 300, "criterion": "gini"},
-    #     #     {"n_estimators": 300, "criterion": "entropy"},
-    #     #     {"n_estimators": 200, "criterion": "gini", "max_leaf_nodes": 179},
-    #     #     {"n_estimators": 200, "criterion": "entropy", "max_leaf_nodes": 179},
+    # ["SVM", SVC, [
+    #     {"gamma": "scale", "kernel": "rbf"},
+    #     {"gamma": "scale", "kernel": "linear"},
     # ]],
+    ["Random Forest", RandomForestClassifier, [
+        # {"n_estimators": 200, "criterion": "entropy", "min_samples_split": 0.01},
+        # {"n_estimators": 200, "criterion": "entropy", "min_samples_split": 0.05},
+        {"n_estimators": 100, "criterion": "gini"},
+        {"n_estimators": 100, "criterion": "entropy"},
+        # {"n_estimators": 200, "criterion": "gini"},
+        #     {"n_estimators": 200, "criterion": "entropy"},
+        #     {"n_estimators": 300, "criterion": "gini"},
+        #     {"n_estimators": 300, "criterion": "entropy"},
+        #     {"n_estimators": 200, "criterion": "gini", "max_leaf_nodes": 179},
+        #     {"n_estimators": 200, "criterion": "entropy", "max_leaf_nodes": 179},
+    ]],
     # ["MLP", MLPClassifier, [
     #     {"hidden_layer_sizes": 5, "activation": "relu",
     #         "solver": "lbfgs", "max_iter": 200},
@@ -138,78 +144,85 @@ classifications = [
 ]
 
 
-def find_best_classifier(datasets, targets, classifications):
+def find_best_classifier(classifications):
     out = pd.DataFrame(
-        columns=["Language", "Dataset", "Target", "Method", "Params", "Accuracy", "Precision", "Recall"])
+        columns=["Dataset", "Method", "Params", "Accuracy", "Precision", "Recall"])
 
     # Iterate datasets
-    for target_id, target in enumerate(targets):
+    for target_id, target in enumerate(['d_fr_eco', 'd_fr_lab', 'd_fr_sec']):
         target_name = target[0]
         y = target[1]
 
-        print("targets: ", str(target_id+1), "/", str(len(targets)))
+        print("targets: ", str(target_id+1), "/", str(3))
+
+        datasets = prepare_datasets(
+            'es',
+            target,
+            sampling,
+            tfidf_maxdf)
 
         for data_id, dataset in enumerate(datasets):
-            name = dataset[0]
+            dataset_name = dataset[0]
             data = dataset[1]
+            y = np.array(dataset[2])
             skf = ShuffleSplit(n_splits=3)
             split_indices = []
 
             for train_index, test_index in skf.split(data, y):
                 split_indices.append((train_index, test_index))
 
-            print("datasets: ", str(data_id+1), "/", str(len(datasets)))
+                print("datasets: ", str(data_id+1), "/", str(len(datasets)))
 
-            # Iterate classifications
-            for cls_id, classification in enumerate(classifications):
-                clf_name = classification[0]
-                clf_params = classification[2]
+                # Iterate classifications
+                for cls_id, classification in enumerate(classifications):
+                    clf_name = classification[0]
+                    clf_params = classification[2]
 
-                print("classifier: ", clf_name, ", ", str(
-                    cls_id+1), "/", len(classifications))
+                    print("classifier: ", clf_name, ", ", str(
+                        cls_id+1), "/", len(classifications))
 
-                # Iterate parametrizations
-                for p_id, param in enumerate(clf_params):
-                    print("Params: ", param, ", ", str(
-                        p_id+1), "/"+str(len(clf_params)))
+                    # Iterate parametrizations
+                    for p_id, param in enumerate(clf_params):
+                        print("Params: ", param, ", ", str(
+                            p_id+1), "/"+str(len(clf_params)))
 
-                    acc_scores = []
-                    pre_scores = []
-                    rec_scores = []
+                        acc_scores = []
+                        pre_scores = []
+                        rec_scores = []
 
-                    # Iterate splits
-                    for train_index, test_index in split_indices:
+                        # Iterate splits
+                        for train_index, test_index in split_indices:
 
-                        X_train, X_test = data[train_index], data[test_index]
-                        y_train, y_test = y[train_index], y[test_index]
+                            X_train, X_test = data[train_index], data[test_index]
+                            y_train, y_test = y[train_index], y[test_index]
 
-                        if len(y.shape) > 1:
-                            clf = MultiOutputClassifier(
-                                classification[1](**param))
-                        else:
-                            clf = classification[1](**param)
-                        try:
-                            clf.fit(X_train, y_train)
-                            y_pred = clf.predict(X_test)
-                            prfs = precision_recall_fscore_support(
-                                y_test, y_pred, warn_for=[])
+                            if len(y.shape) > 1:
+                                clf = MultiOutputClassifier(
+                                    classification[1](**param))
+                            else:
+                                clf = classification[1](**param)
+                            try:
+                                clf.fit(X_train, y_train)
+                                y_pred = clf.predict(X_test)
+                                prfs = precision_recall_fscore_support(
+                                    y_test, y_pred, warn_for=[])
 
-                            acc_scores.append(clf.score(X_test, y_test))
-                            pre_scores.append(prfs[0].mean())
-                            rec_scores.append(prfs[1].mean())
-                        except:
-                            print("Exception during fitting")
-                            acc_scores.append(0)
-                            pre_scores.append(0)
-                            rec_scores.append(0)
+                                acc_scores.append(clf.score(X_test, y_test))
+                                pre_scores.append(prfs[0].mean())
+                                rec_scores.append(prfs[1].mean())
+                            except:
+                                print("Exception during fitting")
+                                acc_scores.append(0)
+                                pre_scores.append(0)
+                                rec_scores.append(0)
 
-                    clf_acc = np.array(acc_scores).mean()
-                    clf_pre = np.array(pre_scores).mean()
-                    clf_rec = np.array(rec_scores).mean()
-                    out = out.append(pd.DataFrame([[language, name, target_name, clf_name, str(
-                        param), clf_acc, clf_pre, clf_rec]], columns=out.columns), ignore_index=True)
+                        clf_acc = np.array(acc_scores).mean()
+                        clf_pre = np.array(pre_scores).mean()
+                        clf_rec = np.array(rec_scores).mean()
+                        out = out.append(pd.DataFrame([[dataset_name, clf_name, str(
+                            param), clf_acc, clf_pre, clf_rec]], columns=out.columns), ignore_index=True)
 
-                out.to_csv("results.csv", index=False)
+                    out.to_csv("results.csv", index=False)
 
     # Final save
     out.to_csv("results.csv", index=False)
@@ -217,4 +230,4 @@ def find_best_classifier(datasets, targets, classifications):
     print("DONE!")
 
 
-find_best_classifier(datasets, targets, classifications)
+find_best_classifier(classifications)
